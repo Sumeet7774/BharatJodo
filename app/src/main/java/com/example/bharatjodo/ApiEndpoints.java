87beb2844b123573b340114f0fa768149c808294
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-08-04 02:37:48 +0530

    Added a VolleySingleton java file to manage network request efficiently. And also added ApiEndpoints java file mentioning the local system Api url.

---
+++ app/src/main/java/com/example/bharatjodo/ApiEndpoints.java
@@ -0,0 +1,6 @@
+package com.example.bharatjodo;
+
+public class ApiEndpoints {
+    private static final String base_url = "https://192.168.29.135/htdocs/BharatJodoApi/";
+    private static final String register_url = base_url + "register.php";
+}
